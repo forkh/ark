6329583a47a02da9a21c3023331e78da409bfdfd
Anders Kiel Hovgaard <akh@outlook.com>
2013-10-07 19:26:27 +0200

    g2

---
+++ G2/addiu.asm
@@ -0,0 +1,14 @@
+addu $s0, $zero, $zero
+addu $s1, $zero, $zero
+
+addiu $s0, $zero, 5
+addiu $s1, $zero, 4
+
+addiu $s0, $zero, 5
+addiu $s1, $zero, 4
+
+addiu $s0, $zero, 5
+addiu $s1, $zero, 4
+
+addiu $s0, $zero, 5
+addiu $s1, $zero, 4
