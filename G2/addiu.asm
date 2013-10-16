6ad93a3bd05a0b37cee855fe19b5b789de8b0f5d
Anders Kiel Hovgaard <akh@outlook.com>
2013-10-16 12:47:04 +0200

    implemented addiu

--- G2/addiu.asm
+++ G2/addiu.asm
@@ -1,14 +1,3 @@
-addu $s0, $zero, $zero
-addu $s1, $zero, $zero
-
-addiu $s0, $zero, 5
-addiu $s1, $zero, 4
-
-addiu $s0, $zero, 5
-addiu $s1, $zero, 4
-
-addiu $s0, $zero, 5
-addiu $s1, $zero, 4
-
+# addiu
 addiu $s0, $zero, 5
 addiu $s1, $zero, 4
